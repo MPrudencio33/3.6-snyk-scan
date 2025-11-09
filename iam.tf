f8ab2c97d1365fce308e76657fc57b86aa52b9f7
zanjero <zanjero3333@gmail.com>
2025-11-10 00:37:49 +0800

    updated files

--- iam.tf
+++ iam.tf
@@ -1,3 +1,4 @@
+/*
 data "aws_iam_policy_document" "inline_policy_cloudwatch" {
   statement {
     actions = [
@@ -8,6 +9,7 @@ data "aws_iam_policy_document" "inline_policy_cloudwatch" {
     resources = ["arn:aws:logs:us-east-1:255945442255:log-group:/aws/lambda/${var.lambda_function_name}:*"]
   }
 }
+*/
 
 data "aws_iam_policy_document" "assume_role" {
   statement {
